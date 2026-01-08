644c0c134ce7df9038857ac7adcfee33df851bd3
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-08 13:55:59 -0500

    enable raw mode

--- nexte.c
+++ nexte.c
@@ -1,8 +1,38 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disable_raw_mode(void) {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enable_raw_mode(void) {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disable_raw_mode);
+
+  struct termios raw = orig_termios;
+
+  /* `ECHO` is a bitflag, defined as 00000000000000000000000000001000 in binary
+   *
+   * We use the bitwise-NOT operator on this value to get
+   * 11111111111111111111111111110111
+   *
+   * We then bitwise-AND this value with the local flags field, which forces the
+   * fourth bit in the flags field to become 0
+   *
+   * Every other bit retains its current value */
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(void) {
+  enable_raw_mode();
+
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1)
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     ;
   return 0;
 }
