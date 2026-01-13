7c71ec8ae426a39e5f0eecc64b8d180a0b1cfe08
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-13 09:43:27 -0500

    we got char printing lets go

--- nexte.c
+++ nexte.c
@@ -34,7 +34,13 @@ int main(void) {
   enable_raw_mode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
+
   return 0;
 }
