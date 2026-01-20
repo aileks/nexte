4c309eee11fc0d98ca523886834eac35d50bd9f8
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-20 10:23:40 -0500

    handle arrow keys

--- nexte.c
+++ nexte.c
@@ -111,7 +111,32 @@ char editorReadKey() {
       die("read");
     }
   }
-  return c;
+
+  if (c == '\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) {
+      return '\x1b';
+    }
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) {
+      return '\x1b';
+    }
+
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+      case 'A':
+        return 'w';
+      case 'B':
+        return 's';
+      case 'C':
+        return 'd';
+      case 'D':
+        return 'a';
+      }
+    }
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
 
 /*
@@ -285,17 +310,17 @@ void editorMoveCursor(char key) {
   case 'd':
     E.cx++;
     break;
-  case 's':
+  case 'w':
     E.cy--;
     break;
-  case 'w':
+  case 's':
     E.cy++;
     break;
   }
 }
 
 // Main input processing loop: read key and handle it.
-void editorProcessKeypress() {
+void editorProcessKeyPress() {
   char c = editorReadKey();
 
   switch (c) {
