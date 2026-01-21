7f01e39c7aa67a1f7f418ce4a96efe73fec1f4ab
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-21 09:14:29 -0500

    only only the cursor to go to the end of the line

--- nexte.c
+++ nexte.c
@@ -446,6 +446,8 @@ void editorRefreshScreen() {
  * Does not check bounds - cursor can move outside visible area.
  */
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
   case ARROW_LEFT:
     if (E.cx != 0) {
@@ -453,7 +455,9 @@ void editorMoveCursor(int key) {
     }
     break;
   case ARROW_RIGHT:
-    E.cx++;
+    if (row && E.cx < row->size) {
+      E.cx++;
+    }
     break;
   case ARROW_UP:
     if (E.cy != 0) {
